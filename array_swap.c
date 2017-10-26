f42b1601838104901af05f36cd06ba930ba472e4
hliu3 <hliu3@stuy.edu>
2017-10-26 10:23:31 -0400

    correcting missing homework. didnt get chance to last night

--- array_swap.c
+++ array_swap.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int a[10];
+  int b[10];
+  int i;
+  int *p;
+  srand(time(NULL));
+  //set and print a
+  for(i = 0; i < 9; i++) {
+    a[i] = rand();
+    printf("a[%d] = %d\n",i,a[i]);
+  }
+  a[9] = 0;
+  printf("a[9] = %d\n\n",a[9]);
+  //set and print b
+  for(i = 0; i < 10; i++) {
+    p = &a[9-i];
+    b[i] = *p;
+    printf("b[%d] = %d\n",i,b[i]);
+  }
+}
